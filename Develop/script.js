4aea7d71ef6f7924f68a6941e09bfab45127d955
Priyam Carto <cpriyam90@gmail.com>
2022-03-17 15:36:23 -0400

    generate password function created

--- Develop/script.js
+++ Develop/script.js
@@ -12,13 +12,18 @@ var number = ['0','1','2','3','4','5','6','7','8','9'];
 
 //Define function to create prompts for user
 function createPrompts () {
+  choice = [];
+
+  //Direct user to select characters between 8-128
   characterLength = parseInt(prompt("Select between 8 to 128 characters for password length"));
 
+  //Return false if number not entered, or number < 8, > 128
   if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
     alert("Character length must be a number between 8-128");
     return false;
   }
 
+  //Prompt user to select if they would like lowercase, uppercase, numbers, special characters in password
   if (confirm("Would you like lowercase letters in your password?")) {
     choice = choice.concat(lowerCase);
   }
@@ -28,24 +33,37 @@ function createPrompts () {
   }
 
   if (confirm("Would you like special characters in your password?")) {
-    choice = choice.concat(specialCharacter.Case);
+    choice = choice.concat(specialCharacter);
   }
 
   if (confirm("Would you like numbers in your password?")) {
     choice = choice.concat(number);
   }
+  return true;
 }
 
-
-
+//Generate password based on user prompts selected
+function generatePassword () {
+  var password = "";
+  for (var i = 0; i < characterLength; i++) {
+      var randomIndex = Math.floor(Math.random() * choice.length);
+      password = password + choice[randomIndex];
+  }
+  return password;
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var correctPrompts = createPrompts();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  if (correctPrompts) {
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
 
+  } else {
+    passwordText.value = "";
+  }
 }
 
 // Add event listener to generate button
